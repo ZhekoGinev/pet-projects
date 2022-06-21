54bbc4cf1576af799905e9ab987b1f4141acb6d1
Zvezdobroeca <jginev@gmail.com>
2022-06-21 16:08:18 +0300

    add README, gitignore and the script itself

---
+++ simple-password-generator/README.md
@@ -0,0 +1,5 @@
+#### Description
+
+This little script will generate a complex password with a random mix of numbers, capital and lowercase letters and special symbols.
+If you chose to save the password it will create a file called "passwords.txt" in the same directory where the script is located.
+And ofcourse you can modify it any way that suits your needs
---
+++ simple-password-generator/password_generator.py
@@ -0,0 +1,36 @@
+from random import choice
+from time import sleep
+from datetime import datetime
+
+character_table = list("0123456789" 
++ "abcdefghijklmnopqrstuvwxyz" 
++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
++ "~`!@#$%^&*()_-+={[}]|\:;\"'<,>.?/")
+
+while True:
+    length = ''
+    while not length.isdigit():
+        length = input("Select password length: ")
+    length = int(length)
+
+    password = ''
+    for _ in range(length):
+        password += choice(character_table)
+
+    print(f"\nPassword generated successfully:  {password}\n")
+    save = ''
+    save = input("Would you like to save the password? [Y/n]: ")
+    print()
+    if save.lower() in ('y', 'yes'):
+        with open('passwords.txt', 'a') as f:
+            time = str(datetime.now())[:19]
+            print(f"{time} - size({length}) - {password}\n----------", file=f)
+    
+    again = ''
+    again = input("Do you wish to generate another password? [Y/n]: ")
+    if again.lower() in ('y', 'yes'):
+        continue
+    else:
+        print("Program terminated")
+        sleep(2)
+        break
