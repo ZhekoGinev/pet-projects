bf5463a6a1af1a4857b38dfc7338221a8a0c0088
Zheko V. Ginev <jginev@gmail.com>
2023-01-17 17:30:54 +0200

    Create git-reaper.py

---
+++ git-reaper/git-reaper.py
@@ -0,0 +1,16 @@
+import re
+import subprocess
+
+
+def delete_remote_branches(pattern):
+    # Get a list of all remote branches
+    branches = subprocess.run(['git', 'branch', '-r'], capture_output=True, text=True).stdout.splitlines()
+
+    # Iterate through the branches and delete the ones that match the pattern
+    for branch in branches:
+        if re.search(pattern, branch):
+            branch = branch.replace("origin/", "").strip()
+            subprocess.run(['git', 'push', 'origin', '--delete', branch], capture_output=True)
+
+# Edit the patter to match branch name
+delete_remote_branches(r"enter_pattern_here")
