32f5c8b90e5d451b1c3acc8dfa39f7504195dfd0
zhekoginev <jginev@gmail.com>
2023-01-23 11:45:38 +0200

    WIP, rewrote the script to: 1. accept arguments when ran, 2. collect the output in a a list and asking before deleting, 3. minor cosmetical/interaction changes

--- git-reaper/git-reaper.py
+++ git-reaper/git-reaper.py
@@ -1,17 +1,85 @@
 import re
 import subprocess
+import argparse
 
 
-def delete_remote_branches(pattern):
+def delete_remote_branches_by_regex(pattern):
+    global deleted
+    pattern = rf"{pattern}"
+    matched = []
     # Get a list of all remote branches
-    branches = subprocess.run(['git', 'branch', '-r'], capture_output=True, text=True).stdout.splitlines()
+    branches = subprocess.run(
+        ['git', 'branch', '-r'], capture_output=True, text=True).stdout.splitlines()
 
-    # Iterate through the branches and delete the ones that match the pattern
+    # Iterate through the branches and get a list of the ones that match the pattern
     for branch in branches:
         if re.search(pattern, branch):
             branch = branch.replace('origin/', '').strip()
-            subprocess.run(['git', 'push', 'origin', '--delete', branch], capture_output=True)
+            matched.append(branch)
+            # subprocess.run(['git', 'push', 'origin', '--delete', branch], capture_output=True)
 
-            
-# Edit the pattern to match branch name
-delete_remote_branches(r'enter_pattern_here')
+    if len(matched) == 0:
+        return False
+
+    print(f"Number of branches found: {len(matched)}\n")
+    for i in matched:
+        print(i)
+    to_delete = input("\nAre you sure you want to delete? [y/n]: ")
+    if to_delete:
+        for br in matched:
+            # subprocess.run(['git', 'push', 'origin', '--delete', br], capture_output=True)
+            print(f"{br} deleted")
+            deleted += 1
+
+
+def delete_remote_branches_by_age(years):
+    global deleted
+    matched = []
+
+    # Get a list of remote branches with age and committer
+    result = subprocess.run(["git", "for-each-ref", "--sort=-committerdate:iso8601",
+                            "--format='%(committerdate:relative)|%(refname:short)|%(committername)'",
+                             "refs/remotes/"], capture_output=True, text=True)
+    result = str(result.stdout).split('\n')
+
+    # Iterate through the branches and append the ones that match the age
+    for branch in result:
+        if f"{years} year" in branch:
+            tokens = branch.split("|")
+            age = tokens[0][1:] #0
+            branch_name = tokens[1] #1
+            branch_name = branch_name.replace("origin/", "")
+            commiter = tokens[2][:-1] #2
+
+            matched.append([age, branch_name, commiter])
+
+    if len(matched) == 0:
+        return False
+
+    print(f"Number of branches found: {len(matched)}\n")
+    for i in matched:
+        print(f"{i[1]}, commited by {i[2]}, {i[0]}")
+    to_delete = input("\nAre you sure you want to delete? [y/n]: ")
+    if to_delete:
+        for br in matched:
+            # subprocess.run(['git', 'push', 'origin', '--delete', br[1]], capture_output=True)
+            print(f"{br[1]} deleted")
+            deleted += 1
+
+
+print("\nReaper is searching...\n")
+
+deleted = 0
+
+# Get argumens from CLI
+parser = argparse.ArgumentParser()
+parser.add_argument("--pattern", dest="pattern", type=str)
+parser.add_argument("--age", dest="age", type=int)
+args = parser.parse_args()
+
+if args.age:
+    delete_remote_branches_by_age(args.age)
+if args.pattern:
+    delete_remote_branches_by_regex(args.pattern)
+
+print(f"\nExecution finished. Branches deleted: {deleted}")
